4bb9afdd503629bcdbd705013f19231e12bafbef
Christian Rodriguez <rodchris3090452@gmail.com>
2023-04-19 20:42:54 -0400

    4-19-2023 nav bvar and routes

--- user-app/src/App.js
+++ user-app/src/App.js
@@ -1,9 +1,17 @@
-import Directory from './components/directory/directory.component'
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
 const App = () => {
 
   return (
-    <Directory />
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+
+      </Route>
+    </Routes>
   )
 }
 
