7cd18a96fd8a3c70b166b7c9caa2ae0e5142b189
Christian Rodriguez <rodchris3090452@gmail.com>
2024-01-22 20:40:09 -0500

    migrated user and category context to use reducers

--- user-app/src/routes/navigation/navigation.component.jsx
+++ user-app/src/routes/navigation/navigation.component.jsx
@@ -1,44 +1,50 @@
 import { Fragment, useContext } from 'react'
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 
-import { UserContext } from '../../context/user.context'
 import { CartContext } from '../../context/cart.context'
+import { selectCurrentUser } from '../../store/user/user.selector'
 
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 
-import { NavigationContainer, LogoContainer, NavLinks, NavLink } from './navigation.styles'
+import {
+    NavigationContainer,
+    LogoContainer,
+    NavLinks,
+    NavLink
+} from './navigation.styles'
 
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext)
+    const currentUser = useSelector(selectCurrentUser)
     const { isCartOpen } = useContext(CartContext)
-
+    
     return (
         <Fragment>
-            <NavigationContainer>
-                <LogoContainer to='/'>
-                    <CrownLogo className='logo' />
-                </LogoContainer>
-                <NavLinks>
-                    <NavLink to='/shop'>
-                        SHOP
-                    </NavLink>
-                    {currentUser ? (
-                        <NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink>)
-                        : (<NavLink to='/auth'>
-                            SIGN IN
-                        </NavLink>)
-                    }
-                    <CartIcon />
-                </NavLinks>
-                {isCartOpen && <CartDropdown />}
-            </NavigationContainer>
-            <Outlet />
+          <NavigationContainer>
+            <LogoContainer to='/'>
+              <CrownLogo className='logo' />
+            </LogoContainer>
+            <NavLinks>
+              <NavLink to='/shop'>SHOP</NavLink>
+    
+              {currentUser ? (
+                <NavLink as='span' onClick={signOutUser}>
+                  SIGN OUT
+                </NavLink>
+              ) : (
+                <NavLink to='/auth'>SIGN IN</NavLink>
+              )}
+              <CartIcon />
+            </NavLinks>
+            {isCartOpen && <CartDropdown />}
+          </NavigationContainer>
+          <Outlet />
         </Fragment>
-    )
-}
-
-export default Navigation
+      );
+    };
+    
+    export default Navigation;
--- user-app/src/routes/shop/shop.component.jsx
+++ user-app/src/routes/shop/shop.component.jsx
@@ -1,18 +1,32 @@
-import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-import CategoriesPreview from '../categories-preview/categories-preview.component'
-import Category from '../category/category.component'
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
-import './shop.styles.scss'
+import { setCategoriesMap } from '../../store/categories/categories.action';
+
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
 const Shop = () => {
-    
-    return (
-      <Routes>
-        <Route index element={<CategoriesPreview />} />
-        <Route path=':category' element={<Category />} />
-      </Routes>
-    )
-}
+  const dispatch = useDispatch();
 
-export default Shop
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments('categories');
+      dispatch(setCategoriesMap(categoryMap));
+    };
+
+    getCategoriesMap();
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
+  );
+};
+
+export default Shop;
